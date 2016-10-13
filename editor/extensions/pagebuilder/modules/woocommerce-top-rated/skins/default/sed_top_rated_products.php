729066ecd369673b55c2d797a833941d5ecc763f
parsaatef <parsaatef@gmail.com>
2016-10-13 11:57:40 -0800

    Remove Woocommerce Modules From Page Builder Core modules & Add To Site Editor Woocommerce Extension

--- editor/extensions/pagebuilder/modules/woocommerce-top-rated/skins/default/sed_top_rated_products.php
+++
@@ -1,2 +0,0 @@
-<?php
- include SED_BASE_PB_APP_PATH . DS . 'modules' . DS . 'woocommerce-archive' . DS . 'includes' . DS . "woo-shortcode-tmpl.php";
--- editor/extensions/pagebuilder/modules/woocommerce-wishlist/skins/default/sed_woocommerce_wishlist.php
+++
@@ -1,3 +0,0 @@
-<div <?php echo $sed_attrs; ?> class="module modules-woocommerce module-woocomerce-wishlist <?php echo $class; ?>">
-    <?php echo do_shortcode('[yith_wcwl_wishlist]'); ?>
-</div>
