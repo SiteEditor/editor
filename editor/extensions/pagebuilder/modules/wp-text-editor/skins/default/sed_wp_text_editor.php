fb1fc3d7b6c599a7cef0fc4c86755f84b261a32a
parsaatef <parsaatef@gmail.com>
2017-04-07 20:05:16 +0430

    Wp Text Editor Fixed issue #

--- editor/extensions/pagebuilder/modules/wp-text-editor/skins/default/sed_wp_text_editor.php
+++ editor/extensions/pagebuilder/modules/wp-text-editor/skins/default/sed_wp_text_editor.php
@@ -1,3 +1,3 @@
 <div <?php echo $sed_attrs; ?> class="module module-wp-text-editor <?php echo $class;?> ">
-    <?php echo sed_js_remove_wpautop( apply_filters( 'sed_pb_builder_module_content', $content ) , true ) ; ?>
+    <?php echo $content; ?>
 </div>
