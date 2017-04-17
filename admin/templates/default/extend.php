9cb677dcb1a118c959df17dc76e562ddd5cddea3
parsaatef <parsaatef@gmail.com>
2017-04-17 09:15:58 +0430

    Remove Extra Admin File

--- admin/templates/default/extend.php
+++ admin/templates/default/extend.php
@@ -7,7 +7,7 @@
             <p>
             <?php
             $etend_url = "http://www.siteeditor.org/extensions";
-            echo sprintf( __('Now SiteEditor is included more than 90 free and premium modules , you can downloaded <a href="%s">here</a>' , 'site-editor') , $etend_url );
+            echo sprintf( __('Now SiteEditor is included more than 90 free and premium modules , you can downloaded <a href="%s">here</a>' , 'site-editor') , esc_url( $etend_url ) );
             ?>
             </p>
         </div>
